9d4e3607bae36f59a1ee5c0310b09857ce53b27f
Petrik Diak <diak@petrik.hu>
2026-02-09 13:29:35 +0100

    Van változtatás, kérünk be számot, Tezsted le Alan!

--- Program.cs
+++ Program.cs
@@ -15,6 +15,27 @@
             return lottoSzamok;
 		}
 
+        static int [] Bekerszamok(int[] lotto)
+        {
+            int[] beker = new int[lotto.Length];
+            for(int i = 0; i < lotto.Length; i++)
+            {
+                Console.Write($"Kérem adja meg a {i + 1}. számot: ");
+                beker[i] = int.Parse(Console.ReadLine());
+                for (int j = 0; j < i; j++)
+                {
+                    if (beker[i] == beker[j])
+                    {
+                        Console.WriteLine("Ez a szám már szerepel, kérem adjon meg egy másikat.");
+                        i--;
+                        break;
+                    }
+				}
+			}
+
+			return beker;
+		}
+
 
 		static void Main(string[] args)
         {
