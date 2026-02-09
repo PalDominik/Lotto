169fed727179fc0f4e2b16381f1bdfdcf1a2fec3
Petrik Diak <diak@petrik.hu>
2026-02-09 13:25:37 +0100

    asd

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Lotto
+{
+    internal class Program
+    {
+
+        static int [] LottoSzamokGeneralasMetodus()
+        {
+            Random random = new Random();
+            int[] lottoSzamok = new int[5];
+            for (int i = 0; i < lottoSzamok.Length; i++)
+            {
+                lottoSzamok[i] = random.Next(1, 91);
+            }
+            Console.WriteLine("A generált lottószámok: " + string.Join(", ", lottoSzamok));
+            return lottoSzamok;
+		}
+
+
+		static void Main(string[] args)
+        {
+            int [] nyeroszamok = LottoSzamokGeneralasMetodus();
+		}
+    }
+}
