1c1461e2afb855fe4c12547350237baef178d5ef
Petrik Diak <diak@petrik.hu>
2026-02-09 14:18:07 +0100

    Lotto feladat teljesen kész

--- Program.cs
+++ Program.cs
@@ -11,35 +11,87 @@
             {
                 lottoSzamok[i] = random.Next(1, 91);
             }
-            Console.WriteLine("A generált lottószámok: " + string.Join(", ", lottoSzamok));
             return lottoSzamok;
 		}
 
-        static int [] Bekerszamok(int[] lotto)
+        static int[] Bekerszamok(int[] lotto)
         {
             int[] beker = new int[lotto.Length];
-            for(int i = 0; i < lotto.Length; i++)
+            int i = 0;
+
+            while (i < lotto.Length)
             {
-                Console.Write($"Kérem adja meg a {i + 1}. számot: ");
-                beker[i] = int.Parse(Console.ReadLine());
+                Console.Write("Kérek egy számot: ");
+                int tipp = Convert.ToInt32(Console.ReadLine());
+                bool vanE = false;
                 for (int j = 0; j < i; j++)
                 {
-                    if (beker[i] == beker[j])
+                    if (beker[j] == tipp)
                     {
-                        Console.WriteLine("Ez a szám már szerepel, kérem adjon meg egy másikat.");
-                        i--;
+                        vanE = true;
                     }
-				}
-			}
-
-			return beker;
+                }
+                if (!vanE)
+                {
+                    beker[i] = tipp;
+                    i++;
+                }
+                else
+                {
+                    Console.WriteLine("Ilyen van már.");
+                }
+            }
+            return beker;
+            //         for(int i = 0; i < lotto.Length; i++)
+            //         {
+            //             Console.Write($"Kérem adja meg a {i + 1}. számot: ");
+            //             beker[i] = int.Parse(Console.ReadLine());
+            //             for (int j = 0; j < i; j++)
+            //             {
+            //                 if (beker[i] == beker[j])
+            //                 {
+            //                     Console.WriteLine("Ez a szám már szerepel, kérem adjon meg egy másikat.");
+            //                     i--;
+            //                 }
+            //	}
+        //  }
 		}
+        static void kiir(int[] tomb)
+        {
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                Console.Write(tomb[i] + " ");
+            }
+            Console.WriteLine();
+        }
 
+        static void TalalatokSzama(int[] lotto, int[]nyero)
+        {
+            int darab = 0;
+            for(int i = 0;i < nyero.Length;i++)
+            {
+                for (int j = 0; j < lotto.Length; j++)
+                {
+                    if (nyero[i] == lotto[j])
+                    {
+                        darab++;
+                    }
+                }
+            }
+			Console.WriteLine($"Találatok száma: {darab}");
+
+        }
 
 		static void Main(string[] args)
         {
             int [] nyeroszamok = LottoSzamokGeneralasMetodus();
+            kiir(nyeroszamok);
             int [] enszamaim = Bekerszamok(nyeroszamok);
+            kiir(enszamaim);
+
+            TalalatokSzama(nyeroszamok, enszamaim);
+
+
 		}   
     }
 }
